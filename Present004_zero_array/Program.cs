a346100f79cd6e12ce0dcff8e8fad88e74912813
Nick <nicknickspb@icloud.com>
2021-10-31 00:08:19 +0300

    Present004_zeroMassive

---
+++ Present004_zero_array/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] a;
+a = new int[10];
+
+int i;
+i = 0;
+while(i<10)
+{
+    a[i] = 0;
+    ++i;
+}
+
+for(int j=0;j<10;++j)
+{
+    a[j]=0;
+}
+
+for(i=0;i<10;++i)
+
+Console.WriteLine(a[i]);
