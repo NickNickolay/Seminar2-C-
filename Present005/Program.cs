fdc38c8126a97f35f41b928fda5c18b158305a1b
Nick <nicknickspb@icloud.com>
2021-10-31 00:33:30 +0300

    Present005_ReadMassiv

---
+++ Present005/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] a;
+a = new int[10];
+
+int i;
+i = 0;
+while(i<10)
+{
+    a[i] = 0;
+    ++i;
+}
+
+
+for(int j=0;j<10;++j)
+{
+    string s = Console.ReadLine();
+    a[j]=Convert.ToInt32(s);
+}
+
+for(i=0;i<10;++i)
+
+Console.Write(a[i] + " ");
